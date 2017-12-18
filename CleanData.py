7e7cdcb27848131c09f80e98a76e9620fa179412
JZ_Zhang <zasek415@gmail.com>
2017-12-18 14:36:57 -0500

    Update CleanData.py

--- CleanData.py
+++ CleanData.py
@@ -11,41 +11,45 @@ header = dataOriginal.first()
 datatail = dataOriginal.filter(lambda x: x!=header)
 
 def cleanBadData(line):
-	flag = True
+        flag = True
 
-	#Complaint Number should not be NULL
-	if len(line[0]) == 0:
-		return False
+        #Complaint Number should not be NULL
+        if len(line[0]) == 0:
+                return False
 
-	#Date should not be NULL
-	if len(line[1]) == 0:
-		return False
-	
-	#Report Date should not be NULL
-	if len(line[5]) == 0:
-		return False
+        #Date should not be NULL
+        if len(line[1]) == 0:
+                return False
 
-	#KY_CD should not be NULL
-	if len(line[6]) == 0:
-		return False
+        #Report Date should not be NULL
+        if len(line[5]) == 0:
+                return False
 
-	#CRM_ATPT_CPTD_CD should not be NULL
+        #KY_CD should not be NULL
+        if len(line[6]) == 0:
+                return False
+
+        #CRM_ATPT_CPTD_CD should not be NULL                                                                                                     55,1          Bot
 	if len(line[10]) == 0:
-		return False
+                return False
 
-	#BORO_NUM and ADDR_CD should not be NULL simultaneously
-	if len(line[13]) == 0 and len(line[14]) == 0:
-		return False
+        #BORO_NUM and ADDR_CD should not be NULL simultaneously
+        if len(line[13]) == 0 and len(line[14]) == 0:
+                return False
 
-	#Occur postition should not be NULL
+        #Occur postition should not be NULL
         if len(line[16]) == 0:
                 return False
-	
-	#Check lat/long
-	if len(line[21]) > 1 and len(line[22]) > 1:
-		if not (40.4<= float(line[21]) <= 40.93 and -74.35 <= float(line[22]) <= -73.69):
-			return False
-	return flag
+
+        #Check lat/long
+        if len(line[21]) > 1 and len(line[22]) > 1:
+                if not (40.4<= float(line[21]) <= 40.93 and -74.35 <= float(line[22]) <= -73.69):
+                        return False
+        return flag
+
+def toCSVLine(line):
+        return ','.join(str(d) for d in line)
 
 filtered = datatail.filter(cleanBadData)
-print("Invalid Record Number: "+str(datatail.count()-filtered.count()))
+lines = filtered.map(toCSVLine)
+lines.saveAsTextFile('cleanedData.csv')
